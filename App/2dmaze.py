f27d506ab04ea094123e2fad1ce952a6aea99682
Brian Stack <58025712+brian-stack@users.noreply.github.com>
2020-02-02 01:35:13 -0800

    Add files via upload
    Fixed a bug in which elevators leading up and down could spawn randomly on the first level.

---
+++ App/2dmaze.py
@@ -0,0 +1,202 @@
+import turtle
+import maze_util
+import math
+
+wn = turtle.Screen()
+wn.bgcolor("peru")
+wn.title("Where are you spongebob?!")
+wn.setup(700, 700)
+wn.tracer(0)
+
+# set the shapes to some picture
+turtle.register_shape("assets/wall.gif")
+turtle.register_shape("assets/FinishSmall.gif")
+turtle.register_shape("assets/playersmall.gif")
+turtle.register_shape("assets/down.gif")
+turtle.register_shape("assets/up.gif")
+turtle.register_shape("assets/lift.gif")
+
+# create pen
+class Pen(turtle.Turtle):
+    def __init__(self, texture):
+        turtle.Turtle.__init__(self)
+        self.penup() # so not drawing anything yet
+        self.shape(texture)
+        self.speed(0)# is the fastest speed 
+
+# create player
+class Player(turtle.Turtle):
+    def __init__(self):
+        turtle.Turtle.__init__(self)
+        self.shape("assets/FinishSmall.gif")
+        self.penup()
+        self.speed(0)
+        self.level = 1
+
+    # directions that the player can go into 
+    def go_up(self):
+        # calculate the spot to move to and see if there is a wall
+        move_to_x = player.xcor()
+        move_to_y = player.ycor() + 24
+
+        # check if the space has a wall
+        if(move_to_x, move_to_y) not in walls:
+            self.goto(move_to_x, move_to_y)
+        
+    def go_down(self):
+        # calculate the spot to move to and see if there is a wall
+        move_to_x = player.xcor()
+        move_to_y = player.ycor() - 24
+
+        # check if the space has a wall
+        if(move_to_x, move_to_y) not in walls:
+            self.goto(move_to_x, move_to_y)
+
+    def go_right(self):
+        # calculate the spot to move to and see if there is a wall
+        move_to_x = player.xcor() + 24
+        move_to_y = player.ycor()
+
+        # check if the space has a wall
+        if(move_to_x, move_to_y) not in walls:
+            self.goto(move_to_x, move_to_y)
+
+    def go_left(self):
+        # calculate the spot to move to and see if there is a wall
+        move_to_x = player.xcor() - 24
+        move_to_y = player.ycor()
+
+        # check if the space has a wall
+        if(move_to_x, move_to_y) not in walls:
+            self.goto(move_to_x, move_to_y)
+    
+    def is_collision(self, other):
+        a = self.xcor() - other.xcor()
+        b = self.ycor() - other.ycor()
+        distance = math.sqrt((a **2) + (b ** 2))
+
+        if distance < 5:
+            return True
+        else:
+            return False
+
+    def go_ascend(self):
+        x, y = player.pos()
+        x = int((x + 288) / 24)
+        y = int(( 288 - y) / 24)
+        print(x, y, (maze.grid[self.level][x][y] == 3 or maze.grid[self.level][x][y] == 5))
+        print(maze.grid[self.level])
+
+        if maze.grid[self.level][x][y] == 3 or maze.grid[self.level][x][y] == 5:
+            self.level = self.level + 1
+            setup_maze(maze.getLevel(self.level))
+
+    
+    def go_descend(self):
+        x, y = player.pos()
+        x = int((x + 288) / 24)
+        y = int(( 288 - y) / 24)
+        print(x, y, (maze.grid[self.level][x][y] == 4 or maze.grid[self.level][x][y] == 5))
+        print(maze.grid[self.level])
+
+        if maze.grid[self.level][x][y] == 4 or maze.grid[self.level][x][y] == 5:
+            self.level = self.level - 1
+            setup_maze(maze.getLevel(self.level))
+
+class EndMe(turtle.Turtle):
+    def __init__(self):
+        turtle.Turtle.__init__(self)
+        self.shape("assets/playersmall.gif")
+        self.penup()
+        self.speed(0)
+
+    def destroy(self):
+        self.goto(2000,2000)
+        self.hideturtle()
+
+# create level setup function
+def setup_maze(level):
+    # Create class instance of Pen
+    wallPen.clearstamps()
+    downStairPen.clearstamps()
+    upStairPen.clearstamps()
+    walls.clear()
+    bothStairsPen.clearstamps()
+
+    wallPen.goto(2000,2000)
+    downStairPen.goto(2000,2000)
+    upStairPen.goto(2000,2000)
+    bothStairsPen.goto(2000,2000)
+    endIt.goto(2000, 2000)
+
+    #print(level)
+    for x in range(len(level)):
+        for y in range(len(level[x])):
+            # get the character at each x, y coordinate
+            # Note the order of y and x in the next line
+            element = level[x][y]
+            
+            # calculate the sreen x, y coordinates
+            screen_x = -288 + (x * 24) # each space is 24 bits wide
+            screen_y = 288 - (y * 24)
+
+            # check if it is a 0 (representing a wall)
+            if element == 0:
+                wallPen.goto(screen_x, screen_y)
+                wallPen.stamp()
+                # add wall, so user doesn't go through walls
+                walls.append((screen_x, screen_y))
+            elif element == 3: #Representing stairs going up
+                upStairPen.goto(screen_x, screen_y)
+                upStairPen.stamp()
+            elif element == 4: #Representing stairs going down
+                downStairPen.goto(screen_x, screen_y)
+                downStairPen.stamp()
+            elif element == 5: #Representing both stairs
+                bothStairsPen.goto(screen_x, screen_y)
+                bothStairsPen.stamp()
+            
+            # if character is a P (representing the player)
+            if element == 2:
+                player.goto(screen_x, screen_y)
+                level[y][x] = 1
+                
+            if element == 9:
+                endIt.goto(screen_x, screen_y)
+        
+
+player = Player()
+
+# Create walls coordinate list
+walls = []
+
+# set up the level
+wallPen = Pen("assets/wall.gif")
+upStairPen = Pen("assets/up.gif")
+downStairPen = Pen("assets/down.gif")
+bothStairsPen = Pen("assets/lift.gif")
+endIt = EndMe()
+
+maze = maze_util.Maze(25,25,25)
+setup_maze(maze.getLevel(1))   # initialize the maze
+
+# keyboard bindings
+turtle.listen()
+turtle.onkey(player.go_left, "Left")
+turtle.onkey(player.go_right, "Right")
+turtle.onkey(player.go_up, "Up")
+turtle.onkey(player.go_down, "Down")
+turtle.onkey(player.go_ascend, "q")
+turtle.onkey(player.go_descend, "a")
+
+# turn off screen updates
+wn.tracer(0)
+
+# main game loop
+while True:
+    # Check for player collision with the end location
+    if player.is_collision(endIt):
+        endIt.destroy()
+        print("Patrick has caught SpongeBob")
+        turtle.bye()
+    wn.update()
---
+++ App/maze_util.py
@@ -0,0 +1,143 @@
+
+import numpy as np
+from random import randint
+import queue
+class Maze:
+    def __init__(self, x,y,z):
+        self.xMax = x
+        self.yMax = y
+        self.zMax = z
+        self.grid = initGrid(x,y,z)
+    
+    def getLevel(self, index):
+        if index < self.zMax+1:
+            return self.grid[index]
+        else:
+            raise IndexError
+
+def isSquare(src, dest, grid):
+    sx, sy , sz = src
+    dx, dy, dz = dest
+
+    #Potential squares, we explicitly disallow square patterns in the grid.
+    if dx + 1 < len(grid) and dy > 0 and (grid[dx+1][dy][dz] + grid[dx+1][dy-1][dz] + grid[dx][dy-1][dz] == 3): #NE
+        return True
+    elif dx > 0 and dy > 0 and (grid[dx-1][dy][dz] + grid[dx-1][dy-1][dz] + grid[dx][dy-1][dz] == 3): #NW
+        return True
+    elif dx + 1 < len(grid) and dy + 1 < len(grid[0]) and (grid[dx+1][dy][dz] + grid[dx+1][dy+1][dz] + grid[dx][dy+1][dz] == 3): #SE
+        return True
+    elif dx > 0 and dy + 1 < len(grid[0]) and (grid[dx-1][dy][dz] + grid[dx-1][dy+1][dz] + grid[dx][dy+1][dz] == 3): #SW
+        return True
+    elif dz + 1 < len(grid[0][0]) and dy > 0 and (grid[dx][dy][dz+1] + grid[dx][dy-1][dz+1] + grid[dx][dy-1][dz] == 3): #N - Up
+        return True
+    elif dz > 0 and dy > 0 and (grid[dx][dy][dz-1] + grid[dx][dy-1][dz-1] + grid[dx][dy-1][dz] == 3): #N - Down
+        return True
+    elif dz + 1 < len(grid[0][0]) and dy+1 < len(grid[0]) and (grid[dx][dy][dz+1] + grid[dx][dy+1][dz+1] + grid[dx][dy+1][dz] == 3): #S - Up
+        return True
+    elif dz > 0 and dy+1 < len(grid[0]) and (grid[dx][dy][dz-1] + grid[dx][dy+1][dz-1] + grid[dx][dy+1][dz] == 3): #S - Down
+        return True
+    elif dx + 1 < len(grid) and dz + 1 < len(grid[0][0]) and (grid[dx][dy][dz+1] + grid[dx+1][dy][dz+1] + grid[dx+1][dy][dz] == 3): #E - Up
+        return True
+    elif dx + 1 < len(grid) and dz  > 0  and (grid[dx][dy][dz-1] + grid[dx+1][dy][dz-1] + grid[dx+1][dy][dz] == 3): #E - Down
+        return True
+    elif dx > 0 and dz + 1 < len(grid[0][0]) and (grid[dx][dy][dz+1] + grid[dx-1][dy][dz+1] + grid[dx-1][dy][dz] == 3 ): #W - Up
+        return True
+    elif dx > 0 and dz > 0 and (grid[dx][dy][dz-1] + grid[dx-1][dy][dz-1] + grid[dx-1][dy][dz] == 3): #W - Down
+        return True
+    else:
+        return False
+
+def getAdj(cell, grid, visited):
+    adj = list()
+    x, y, z = cell
+
+    #Potential neighbours
+    east = (x+1, y, z)
+    west = (x-1, y, z)
+    north = (x, y-1, z)
+    south = (x, y+1, z)
+    up = (x,y,z + 1)
+    down = (x, y, z-1)
+
+    #Add qualifying neighbours to list of adjacent
+    if x + 1 < len(grid) and east not in visited and not isSquare(cell, east, grid):
+        adj.append(east)
+    if y + 1 < len(grid) and south not in visited and not isSquare(cell, south, grid):
+        adj.append(south) 
+    if x > 0 and west not in visited and not isSquare(cell, west, grid):
+        adj.append(west) 
+    if y > 0 and north not in visited and not isSquare(cell, north, grid):
+        adj.append(north) 
+    if z + 1 < len(grid) and up not in visited and not isSquare(cell, up, grid):
+        adj.append(up) 
+    if z > 0 and down not in visited and not isSquare(cell, down, grid):
+        adj.append(down) 
+    return adj
+
+def initGrid(xMax, yMax, zMax):
+    grid = np.zeros(shape=(xMax,yMax,yMax), dtype = np.int8)
+
+    toVisit = list() #The stack of cells to visit
+    visited = set()  #Set of cells that have been visited already
+    start = (0, 0, 0)
+    visited.add(start)
+    toVisit.append(start)
+    grid[start[0]][start[1]][start[2]] = 1
+
+    while len(toVisit) != 0:
+        cell = toVisit.pop()
+        adjN = getAdj(cell, grid, visited)
+
+        #The current cell has some unvisited neighbour
+        if len(adjN) != 0:
+            toVisit.append(cell)
+            other = adjN[randint(0, len(adjN)-1)]
+            grid[other[0]][other[1]][other[2]] = 1
+            visited.add(other)
+            toVisit.append(other)
+
+
+    #Mark vertical paths.
+    # 3 - can go up from current cell
+    # 4 - can go down from current cell
+    # 5 - can go either up or down. 
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            for k in range(len(grid[0][0])):
+                if grid[i][j][k] > 0:
+                    if i > 0 and i + 1 < len(grid) and grid[i-1][j][k] > 0 and grid[i+1][j][k] > 0 and randint(0,50) == 0:
+                        grid[i][j][k] = 5
+                        if grid[i-1][j][k] == 1:
+                            grid[i-1][j][k] = 3
+                        if i < len(grid) and grid[i+1][j][k] == 1:
+                            grid[i+1][j][k] = 4
+                    elif i > 0 and grid[i-1][j][k] > 0 and randint(0,50) ==  0:
+                        grid[i][j][k] = 4
+                        if i > 0 and grid[i-1][j][k] == 1:
+                            grid[i-1][j][k] = 3
+                    elif i + 1 < len(grid) and grid[i+1][j][k] > 0 and randint(0,50) == 0:
+                        grid[i][j][k] = 3
+                        if i < len(grid):
+                            if grid[i+1][j][k] == 1:
+                                grid[i+1][j][k] = 4
+                            else:
+                                grid[i+1][j][k] = 5
+    #Mark entrance and exit
+    grid[xMax-1][yMax-1][zMax-1] = 9
+    grid[0][0][0] = 2
+    
+    #This is not an ideal way of padding the array with 0's
+    newGrid = np.zeros(shape=(xMax+2,yMax+2,yMax+2), dtype = np.int8)
+    for i in range(len(newGrid)):
+        for j in range(len(newGrid[0])):
+            for k in range(len(newGrid[0][0])):
+                if i != 0 and j != 0 and k != 0 and i != (len(newGrid) - 1) and j != (len(newGrid[0]) - 1) and k != (len(newGrid[0][0]) - 1):
+                    newGrid[i][j][k] = grid[i-1][j-1][k-1]
+    
+    return newGrid
+
+
+
+
+
+
